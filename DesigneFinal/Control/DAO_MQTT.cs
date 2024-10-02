1de1bbb1ab34350c72db7adb611bb0d5d2a69bc7
Taha <taha.khelifi@saintmichelannecy.fr>
2024-10-02 15:56:19 +0200

    bibou

---
+++ DesigneFinal/Control/DAO_MQTT.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesigneFinal.Control
+{
+    internal class DAO_MQTT
+    {
+        //172.31.254.114
+        //1883
+
+
+    }
+}
